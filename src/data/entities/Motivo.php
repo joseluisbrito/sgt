585ca6b8e5bbc76eb56e38c6e4ef93c401c6d56f
Jlbrito <joseluisbrito@gmail.com>
2021-02-10 00:19:14 -0300

    Updated entity Motivo

--- src/data/entities/Motivo.php
+++ src/data/entities/Motivo.php
@@ -2,12 +2,57 @@
 
 namespace Entities;
 
+// src/persistence/entities/Motivo.php
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="motivos")
+ */
 class Motivo
 {
+    /**
+     * Id
+     * @var id int
+     */
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
     protected $id;
     
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $movito;
+    
+    
+    /**
+     * 
+     @ORM\ManyToOne(targetEntity="Departamento", inversedBy="atiende")
+     */
+    protected $departamentoAtiende;
+    
+    
+    /**
+     * Return current id
+     * @return int
+     */
     public function getId()
     {
         return $this->id;
     }
+    
+    
+    function getMovito() {
+        return $this->movito;
+    }
+
+    function setMovito($movito): void {
+        $this->movito = $movito;
+    }
+
+
 }
